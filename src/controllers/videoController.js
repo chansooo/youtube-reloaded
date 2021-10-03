d19e9cbd004f60351c07e123dd6b11d7387790f9
chansooo <kcs05008@gmail.com>
2021-10-03 22:57:39 +0900

    start making youtube

---
+++ src/controllers/videoController.js
@@ -0,0 +1,27 @@
+
+
+export const trending = (req, res) => {
+    const videos = [{
+        title: "Frist Video",
+        rating: 5,
+        comments:2,
+        createdAt:"2 minutes ago",
+        views:59,
+        id:1,
+    },
+    {
+        title: "Second video",
+        rating: 5,
+        comments:2,
+        createdAt:"2 minutes ago",
+        views:59,
+        id:1,
+    }];
+    res.render("home", {pageTitle: "Home", videos});
+} 
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => res.send("Delete Video");
+
