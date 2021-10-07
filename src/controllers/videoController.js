c5ae760b754740ffa83a300192c31010c12acb6b
chansooo <kcs05008@gmail.com>
2021-10-08 00:02:11 +0900

    developing

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,27 +1,59 @@
 
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-    const videos = [{
-        title: "Frist Video",
-        rating: 5,
-        comments:2,
-        createdAt:"2 minutes ago",
-        views:59,
-        id:1,
-    },
-    {
-        title: "Second video",
-        rating: 5,
-        comments:2,
-        createdAt:"2 minutes ago",
-        views:59,
-        id:1,
-    }];
-    res.render("home", {pageTitle: "Home", videos});
-} 
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+/*Video.find({}, (error, videos) => {
+    if(error){
+        return lsdkslmk
+    }
+    return res.render wlkfenweofn
+});
+*/
+
+export const home = async (req, res) => {
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos});
+};
+
+export const watch = (req, res) => {
+    const { id } = req.params;
+    return res.render("watch", {pageTitle:"Watching"});
+};
+
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    return res.render("edit", {pageTitle: `Editing: `});
+};
+
+export const postEdit = (req,res) => {
+    const { id } = req.params;
+    const {title} = req.body;
+    return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+    return res.render("upload", {pagetitle: "Upload Video"});
+};
+
+export const postUpload = async (req, res) => {
+    const {title, description, hashtags} = req.body;
+
+    try{
+        await Video.create({
+            title,
+            description,
+            createdAt: Date.now(),
+            hashtags: hashtags.split(",").map((word) => `#${word}`),
+            meta: {
+                views: 0,
+                rating: 0,
+            },
+        });
+        return res.redirect("/");
+    } catch(error){
+        return res.render("upload", {pageTitle: "Uplaod Video", errorMessage: error._message,});
+    }
+    
+};
+
+
 
