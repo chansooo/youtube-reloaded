c918dc4198a49ddf8d64283e708a0a66a2c3a239
root <root@DESKTOP-V5R7D1D.localdomain>
2021-10-24 17:55:48 +0900

    make githublogin

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -54,11 +54,27 @@ export const postLogin = async (req, res) => {
             errorMessage:"Wrong Password!",
         });
     }
+    
+    req.session.loggedIn = true;
+    req.session.user = user;
     return res.redirect("/");
 };
 
-req.session.loggedIn = true;
-req.session.user = user;
+export const startGithubLogin = (req, res) =>{
+    const baseUrl="https://github.com/login/oauth/authorize";
+    const config = {
+        client_id: "3ffaddfde69c49d49d45",
+        allow_signup: false,
+        scope: "read:user user:email",
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = (req, res) =>{
+
+};
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
